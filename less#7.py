354a04b9f5f4a457f5340f11063418563a42a807
Chita199853 <lalka417@gmail.com>
2023-01-12 17:08:00 +0300

    Ready less#8

--- less#7.py
+++ less#7.py
@@ -1,23 +1,2 @@
-import math
-
-pow_1 = 1 ** 100
-pow_2 = 2 ** 16
-mins = 2 - 1000
-res_1 = 2 + 2 * 2
-delim = 60 / 3
-cor = math.sqrt(144)
-x_1 = 2 + (5 * 14)
-x_2 = 66 -11
-x_3 = math.sqrt(16 - 8)
-res_line = """
-              1){}
-              2){}
-              3){}
-              4){} 
-              5){}
-              6){}
-              7){}
-              8){}
-              9){}  
-           """.format(pow_1, pow_2, mins, res_1, delim, cor, x_1, x_2, x_3)
-print(res_line)
+s = False
+print(type(str(s)))
