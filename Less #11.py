7b01934c94f43246b529b60ddc679fa254a9b4c2
Chita199853 <lalka417@gmail.com>
2023-01-12 18:56:04 +0300

    Add Calculator!

---
+++ Less #11.py
@@ -0,0 +1,21 @@
+print("Приветствуем вас в нашем калькуляторе! Выберите что вам нужно сделать ! : ")
+try:
+    change_ual = int(input("1)+ \n 2)-\n 3)/ \n 4)*\n"))
+    a = int(input("Число 1 : \n"))
+    b = int(input("Число 2 : \n"))
+    if change_ual == 1:
+        print("Sum:{}".format(a+b))
+    elif change_ual == 2:
+        print("Minus_res: {}".format(a-b))
+    elif change_ual == 3:
+        print("Del: {}".format(a/b))
+    elif change_ual == 4:
+        print("Umn_res: {}".format(a * b))
+    else:
+        print("Не верно выбрано условие! ")
+except ZeroDivisionError:
+    print("Делить на 0 нельзя! ")
+except TypeError:
+    print("Мы работаем только с числами :)")
+except ValueError:
+    print("Не верно выбрано условие, или не корректно указано значение переменных! ")
