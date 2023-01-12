67ecc7fe101659fc8a1f1d9aed998bbc461b5987
Chita199853 <lalka417@gmail.com>
2023-01-12 21:37:20 +0300

    add strReverser

--- Lesss #12.py
+++ Lesss #12.py
@@ -1,7 +1,12 @@
-inp_val = str("58903")
+"""inp_val = str("58903")
 tpls_char = set()
 
 for char in inp_val:
     tpls_char.add(char)
 tpls_char = sorted(tpls_char)
-print("Max value : {}".format(tpls_char[-1]))
+print("Max value : {}".format(tpls_char[-1]))"""
+
+"""inp_val = input("Enter number: ")
+print(inp_val)
+print(inp_val[::-1])"""
+
