be8614382bbad85673fe33a28ff51ca7e3c36fd8
Chita199853 <lalka417@gmail.com>
2023-01-12 11:50:21 +0300

    Add variable

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("hello !")
+var1 = 1
+var1_1 = 22
+var2 = 2.2
+var2_1 = 2231.1123
+var3 = "help"
+var3_1 = "me"
+var4 = True
+var4_1 = False
+var5 = None
+
+res_line =  """ Int : {} and {};
+                Float : {} and {};
+                String : {} and {};
+                Bool   : {} and {};
+                Noone  : {};
+            """.format(var1, var1_1, var2, var2_1, var3, var3_1, var4, var4_1, var5)
+print(res_line)
