744b7fa0a721fb346b2f232ae92dfd853315a383
Chita199853 <lalka417@gmail.com>
2023-01-13 13:14:34 +0300

    taprl getter converter :DDDDDDDDDDdd

---
+++ less #17.py
@@ -0,0 +1,9 @@
+"""string = "Дана строка. Сохранить в ней только первые вхождения символов, удалив все остальные. Результат вывести в виде кортежа."
+splited = string.split(" ")
+total_word_list = []
+for i in splited:
+    total_word_list.append(i[0])
+tpl_lword = tuple(total_word_list)
+print(tpl_lword)"""
+
+
