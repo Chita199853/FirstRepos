d27eee930a79cb89ac2a4d595477c4ba35483ed0
Chita199853 <lalka417@gmail.com>
2023-01-13 18:44:31 +0300

    get max count values in text

--- less #19.py
+++ less #19.py
@@ -2,3 +2,18 @@
 comp_2 = {"qiwi", "yandex", "google", "paypal"}
 print(comp_1&comp_2)"""
 
+"""stroka = "В единственной строке записан текст. Для каждого слова из данного текста подсчитайте, сколько раз оно встречалось в этом тексте ранее. Словом считается последовательность непробельных символов, идущих подряд. Слова разделены одним или большим числом пробелов или символами конца строки."
+dict_counter = dict()
+splited_text = stroka.replace(".", "").replace(",","").split(" ")
+for i in splited_text:
+    if dict_counter.get(i) == None:
+        dict_counter[i] = 1
+    else:
+        dict_counter[i] = dict_counter.get(i) + 1
+list1 = []
+for key,val in dict_counter.items():
+    res_values = val,key
+    list1.append((val,key))
+list1.sort()
+print("Max count in text : {} - {} разА".format(max(list1)[1],max(list1)[0]))"""
+
