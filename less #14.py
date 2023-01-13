26cd8f4c93f6c046ff68ba6df8141c2cc5c5e695
Chita199853 <lalka417@gmail.com>
2023-01-13 10:55:28 +0300

    cheker number < sr_arifmet ?

---
+++ less #14.py
@@ -0,0 +1,21 @@
+"""list = []
+summ_plus = 0
+for i in range(-15,15,1):
+    list.append(i)
+
+for j in list:
+    if j % 2 == 0:
+        if j > 0:
+            summ_plus += j
+print(summ_plus)"""
+
+"""list_num = []
+for i in range(1, 180, 1):
+    list_num.append(i)
+sum_sr_arifm = sum(list_num)/len(list_num)
+for j in list_num:
+    if j < sum_sr_arifm:
+        print("{} - нам подходит! Оно меньше! ".format(j))
+"""
+
+
