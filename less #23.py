cb0116d3742cb1851e0448f77875b8f942b41344
Chita199853 <lalka417@gmail.com>
2023-01-14 19:02:49 +0300

    arr gener

---
+++ less #23.py
@@ -0,0 +1,9 @@
+"""res_array = []
+first = int(input("Firs number: "))
+last = int(input("Last number: "))
+def gen(firs,last):
+    for i in range(firs,last):
+        res_array.append(i)
+gen(first,last)
+print(res_array)"""
+
