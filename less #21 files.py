b427bc22adbad817f917ea5e899aef5256a041ad
Chita199853 <lalka417@gmail.com>
2023-01-14 11:28:52 +0300

    text cheker adedd

--- less #21 files.py
+++ less #21 files.py
@@ -15,3 +15,23 @@
         if inpit_data == "":
             break"""
 
+"""count_word_in_line  = []
+count_simv_in_text = []
+line_iter = 1
+with open("news.txt","r",encoding="utf-8") as f:
+    word_in_line = []
+    simv_in_line = []
+    for line in f:
+        splited_line = line.replace(","," , ").replace(".", " . ").replace("  ", " ").replace("\n", " ").split(" ")
+        for word in splited_line:
+            if word.isalpha() or word.find("-") != -1 and word != '':
+                word_in_line.append(word)
+            else:
+                simv_in_line.append(word)
+
+        print("В строке {} = Слов : {} / Символов : {} ".format(line_iter, len(word_in_line), len(simv_in_line)))
+        line_iter +=1
+        word_in_line.clear()
+        simv_in_line.clear()
+    print("Строк в файле : {}".format(line_iter-1))"""
+
