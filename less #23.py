87d8daad47951594d73f30857609550aff62289d
Chita199853 <lalka417@gmail.com>
2023-01-14 20:15:59 +0300

    add S finder krug,treug,prymoug

--- less #23.py
+++ less #23.py
@@ -7,3 +7,38 @@ def gen(firs,last):
 gen(first,last)
 print(res_array)"""
 
+"""i_num = abs(int(input("Enter number: ")))
+def razr(i_num):
+    counter = 1
+    i_num //= 10
+    while i_num > 0 :
+        i_num //= 10
+        counter +=1
+    return print("Razradov :  {}".format(counter))
+razr(i_num)"""
+
+"""def krug():
+    radius = int(input("Введите радиус круга : "))
+    s = 3,14 * radius ** 2
+    return print(s)
+def prymougol():
+    a = int(input("Введите значение стороны a:"))
+    b = int(input("Введите значение стороны b:"))
+    s = a * b
+    return print(s)
+def treugol():
+    a = int(input("Введите значение стороны основания (a) : "))
+    h = int(input("Введите значение высоты, проведенной к оснаванию : "))
+    s = 0,5 * a * h
+    return print(s)
+while True:
+    vibor = int(input("Выберите площадь какой игуры вычислить : \n1 - круг\n2 - прямоугольник\n3 - треугольник\n"))
+    if vibor == 1:
+        krug()
+    elif vibor == 2:
+        prymougol()
+    elif vibor == 3:
+        treugol()
+    else:
+        break"""
+
