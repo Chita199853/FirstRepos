d1e2c30d4cda1596e90c3abd6f344d73422fed2b
Chita199853 <lalka417@gmail.com>
2023-01-14 10:29:16 +0300

    add dile writer

--- less #21 files.py
+++ less #21 files.py
@@ -1,4 +1,4 @@
-with open("book.txt","r",encoding="utf-8") as f:
+"""with open("book.txt","r",encoding="utf-8") as f:
     total_ball = []
     for line in f:
         slited_line = line.replace("\n", "").split(":")
@@ -6,3 +6,12 @@ with open("book.txt","r",encoding="utf-8") as f:
             print("Двоечник ! : {}".format(line))
         total_ball.append(int(slited_line[-1]))
     print("Ball po clasu : {}".format(sum(total_ball)/len(total_ball)))
+"""
+
+"""with open("file.txt","w") as f:
+    while True:
+        inpit_data = input("ведите строку (что бы закончить ввод - нажимте Enter): ")
+        f.write(inpit_data + "\n")
+        if inpit_data == "":
+            break"""
+
