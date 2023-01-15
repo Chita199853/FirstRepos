0198857cd1f8dea79830324a2b9182ee763b3001
Chita199853 <lalka417@gmail.com>
2023-01-15 20:15:12 +0300

    Create Animal informer

---
+++ less #29.py
@@ -0,0 +1,15 @@
+"""class Animal:
+    def __init__(self, name, age, sex):
+        self.name = name
+        self.age = age
+        self.sex = sex
+    def Namer(self):
+        print("Животное зовут : " + self.name)
+    def Ager(self):
+        print("Возраст животного : " + str(self.age))
+    def Sexer(self):
+        print("Он " + self.sex)
+Dog = Animal("Барсик", "5 лет ", "мальчик!")
+Dog.Namer()
+Dog.Ager()
+Dog.Sexer()"""
