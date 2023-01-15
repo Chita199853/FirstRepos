89d2035626b2215278e3ddfe87e7ff8cda34d91a
Chita199853 <lalka417@gmail.com>
2023-01-15 20:02:41 +0300

    Add class "Namer"

---
+++ less #28.py
@@ -0,0 +1,12 @@
+"""class Person:
+    def __init__(self, age, name):
+        self.age = age
+        self.name = name
+        print("Hello {}, тебе : {}  ".format(self.name, self.age))
+
+P1 = Person(24, "Andreu")
+P2 = Person(33, "Vasya")
+P3 = Person(35, "Igor")
+P4 = Person(45, "Vitya")
+P5 = Person(14,"Inna")"""
+
