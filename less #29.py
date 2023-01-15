23daf36f9c81bb7d748aa84e161529f8202b4b00
Chita199853 <lalka417@gmail.com>
2023-01-15 22:28:26 +0300

    Test incpsulatshion

--- less #29.py
+++ less #29.py
@@ -1,51 +1,63 @@
-class Animal:
-    def __init__(self, name, age, sex):
-        self.name = name
-        self.age = age
-        self.sex = sex
-    def Namer(self):
-        print("Животное зовут : " + self.name)
-    def Ager(self):
-        print("Возраст животного : " + str(self.age))
-    def Sexer(self):
-        print("Он(а) " + self.sex)
-Dog = Animal("Барсик", "5 лет ", "мальчик!")
-Dog.Namer()
-Dog.Ager()
-Dog.Sexer()
+# class Animal:
+#     def __init__(self, name, age, sex):
+#         self.__name = name
+#         self.age = age
+#         self.sex = sex
+#     def Namer(self):
+#         print("Животное зовут : " + self.name)
+#     def Ager(self):
+#         print("Возраст животного : " + str(self.age))
+#     def Sexer(self):
+#         print("Он(а) " + self.sex)
+# P = Animal("Vasy", 33, "He")
+# print(P.name)
 
-class Cat(Animal):
-    def __init__(self, name, age, sex):
-        super().__init__(name, age, sex)
-    def kiskis(self):
-        print("Котик делает мяу")
-print()
-Murzik = Cat("Murzik", 2, "Мальчик")
-Murzik.Namer()
-Murzik.Ager()
-Murzik.Sexer()
-Murzik.kiskis()
 
-class cow(Animal):
-    def __init__(self, name, age, sex):
-        super().__init__(name, age, sex)
-    def mu(self):
-        print("Корова делает Муууууу")
-print()
-Marusya = cow("Marusya", 10 , "девочка")
-Marusya.Namer()
-Marusya.Ager()
-Marusya.Sexer()
-Marusya.mu()
+# Dog = Animal("Барсик", "5 лет ", "мальчик!")
+# Dog.Namer()
+# Dog.Ager()
+# Dog.Sexer()
+#
+# class Cat(Animal):
+#     def __init__(self, name, age, sex):
+#         super().__init__(name, age, sex)
+#     def kiskis(self):
+#         print("Котик делает мяу")
+# print()
+# Murzik = Cat("Murzik", 2, "Мальчик")
+# Murzik.Namer()
+# Murzik.Ager()
+# Murzik.Sexer()
+# Murzik.kiskis()
 
-class Pig(Animal):
-    def __init__(self, name, age, sex):
-        super().__init__(name, age, sex)
-    def hru(self):
-        print("Свинка делает хрю-хрю!")
-print()
-Dusya = Pig("Дуся", "3", "девочка!")
-Dusya.Namer()
-Dusya.Ager()
-Dusya.Sexer()
-Dusya.hru()
+
+# class Pig(Animal):
+#     def __init__(self, name, age, sex):
+#         super().__init__(name, age, sex)
+#     def hru(self):
+#         print("Свинка делает хрю-хрю!")
+# print()
+# Dusya = Pig("Дуся", "3", "девочка!")
+# Dusya.Namer()
+# Dusya.Ager()
+# Dusya.Sexer()
+# class cow(Animal):
+#     def __init__(self, name, age, sex):
+#         super().__init__(name, age, sex)
+#     def mu(self):
+#         print("Корова делает Муууууу")
+# print()
+# Marusya = cow("Marusya", 10 , "девочка")
+# Marusya.Namer()
+# Marusya.Ager()
+# Marusya.Sexer()
+# Marusya.mu()
+# Dusya.hru()
+
+
+
+
+
+
+
+
