0cd72174ac09531517cef136f7316dec922001d2
Chita199853 <lalka417@gmail.com>
2023-01-16 11:17:23 +0300

    ADD CLASS DOGGER

---
+++ less #34.py
@@ -0,0 +1,36 @@
+# from abc import  abstractstaticmethod
+#
+# class Animal():
+#     def __init__(self):
+#         pass
+#
+#     @abstractstaticmethod
+#     def name(self):
+#         pass
+#     @abstractstaticmethod
+#     def vol(self):
+#         pass
+#     @abstractstaticmethod
+#     def age(self):
+#         pass
+#     @abstractstaticmethod
+#     def tip(self):
+#         pass
+#
+# class Dog(Animal):
+#     def __init__(self):
+#         pass
+#     def name(self):
+#         print("Namе : Barsik")
+#     def vol(self):
+#         print("Издает звук :Gav gav")
+#     def age(self):
+#         print("Vozrast : 15")
+#     def tip(self):
+#         print("Разновидность : Собака")
+#
+# Haski = Dog()
+# Haski.tip()
+# Haski.name()
+# Haski.age()
+# Haski.vol()
