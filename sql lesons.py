40dd12e9004befb809b578a26ddc64dfe380ef61
Chita199853 <lalka417@gmail.com>
2023-02-22 15:33:11 +0300

    Add daata in base

--- sql lesons.py
+++ sql lesons.py
@@ -8,7 +8,15 @@ zapr_3 = """CREATE TABLE IF NOT EXISTS INFO_CONF (ConfName TEXT, Time TEXT,Locat
 curs.execute(zapr_1)
 curs.execute(zapr_2)
 curs.execute(zapr_3)
+
+send_data1 = """INSERT INTO INFO_Personal ('Alex', 'Yanushkevich', 'Alexeevich', 'Magistr', 'IT', "Minsk', 'IT', "Uchitel', 'BY', 'Minsk,Nemiga,32', '+375299550909', 'lalka417@gmail.com');"""
+send_data2 = """INSERT INTO INFO_UCHASTNIKOV ('Docladchik', '02/02/2020', 06/02/2020', 'Python', 'None', '22/05/2020', 'No', '22/05/2020');"""
+send_data3 = """INSERT INTO INFO_CONF ('IT World', '05/05/2020', 'Minsk', 'Wargaming', 'VkTeam', '1', '500','34');"""
+curs.execute(send_data1)
+curs.execute(send_data2)
+curs.execute(send_data3)
 conn.commit()
 
 curs.close()
 conn.close()
+input()
